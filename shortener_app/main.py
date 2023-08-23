f263a02a998b527ff105ecc5fffd02803e996046
JHorrigan <jahorrigan1411@gmail.com>
2023-08-23 12:58:09 +0100

    Write URL Model To Database

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -1,11 +1,25 @@
 # shortener_app/main.py
 
-import validators
-from fastapi import FastAPI, HTTPException
+import secrets
 
-from . import schemas
+import validators
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+
+from . import models, schemas
+from .database import SessionLocal, engine
 
 app = FastAPI()
+# Bind your database engine, will create if doesnt exist
+# with all modelled tables
+models.Base.metadata.create_all(bind=engine)
+
+def get_db(): # Create & yield a new db session with each request
+    db = SessionLocal()
+    try:
+        yield db
+    finally: # Close the connection whether an error occurs or not
+        db.close()
 
 def raise_bad_request(message):
     raise HTTPException(status_code=400, detail=message)
@@ -14,10 +28,29 @@ def raise_bad_request(message):
 def read_root():
     return "Welcome to the Short URL API :)"
 
-@app.post("/url") # Post a valid target url
-def create_url(url: schemas.URLBase):
+@app.post("/url", response_model=schemas.URLInfo) # Post a valid target url
+def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
+    # By passing get_db to Depends, this request will use and close a db session
     # pydantic checks if URL is a string, but not if it is a valid url
     # So here we use validators
     if not validators.url(url.target_url):
         raise_bad_request(message="Your provided URL is not valid")
-    return f"TODO: Create database entry for: {url.target_url}"
+
+    # Provide random strings for key & secret_key
+    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    key = "".join(secrets.choice(chars) for _ in range(5))
+    secret_key = "".join(secrets.choice(chars) for _ in range(8))
+
+    # Create a database entry for the target url
+    db_url = models.URL(
+        target_url=url.target_url, key=key, secret_key=secret_key
+    )
+    db.add(db_url)
+    db.commit()
+    db.refresh(db_url)
+
+    # Add key and secret key to match required URLInfo schema for response
+    db_url.url = key
+    db_url.admin_url = secret_key
+
+    return db_url
